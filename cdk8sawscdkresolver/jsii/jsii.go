3de923cdfbd5017f8e03e04d4f35dcfd7b60ab09
cdk8s-automation <cdk8s-team@amazon.com>
2025-12-18 12:11:41 +0000

    chore(release): v0.0.434

--- cdk8sawscdkresolver/jsii/jsii.go
+++ cdk8sawscdkresolver/jsii/jsii.go
@@ -14,7 +14,7 @@ import (
 	cdk8s      "github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2/jsii"
 )
 
-//go:embed cdk8s-awscdk-resolver-0.0.433.tgz
+//go:embed cdk8s-awscdk-resolver-0.0.434.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -26,5 +26,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("@cdk8s/awscdk-resolver", "0.0.433", tarball)
+	_jsii_.Load("@cdk8s/awscdk-resolver", "0.0.434", tarball)
 }
